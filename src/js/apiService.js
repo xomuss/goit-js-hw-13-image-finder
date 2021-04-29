b6bf5c6f1d5d8e425422929cb6833c44dde0c39c
Alexander Khomenko <72103285+xomuss@users.noreply.github.com>
2021-04-29 23:02:55 +0300

    fix

---
+++ src/js/apiService.js
@@ -0,0 +1,31 @@
+export default class ApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+  }
+
+  fetchArticles() {
+    const key = '21316694-ed9f7523baf223edee906888b';
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${key}`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.page += 1;
+
+        return data.hits;
+      });
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+}
---
+++ src/js/project.js
@@ -0,0 +1,41 @@
+import ApiService from './apiService';
+import cardsTpl from '../templates/cardsTpl.hbs';
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.js-container'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
+};
+
+console.log(refs.galleryContainer);
+
+const apiService = new ApiService();
+
+refs.form.addEventListener('submit', onSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  clearContainer();
+  apiService.query = e.currentTarget.elements.query.value;
+  apiService.resetPage();
+  apiService.fetchArticles().then(appendHitsMarkup);
+}
+
+function onLoadMore() {
+  apiService.fetchArticles().then(appendHitsMarkup);
+  window.scrollTo({
+    top: 100,
+    left: 500,
+    behavior: 'auto',
+  });
+}
+
+function appendHitsMarkup(hits) {
+  refs.galleryContainer.insertAdjacentHTML('beforeend', cardsTpl(hits));
+}
+
+function clearContainer() {
+  refs.galleryContainer.innerHTML = '';
+}
